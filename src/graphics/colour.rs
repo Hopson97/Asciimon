e8a47bcffc092e7f196ee1f4a9385093d1364439
Hopson97 <mhopson@hotmail.co.uk>
2018-07-13 19:32:19 +0100

    Create colour strut

---
+++ src/graphics/colour.rs
@@ -0,0 +1,13 @@
+pub struct Colour {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8
+}
+
+impl Colour {
+    pub fn new(r: u8, g: u8, b: u8) -> Colour {
+        Colour {
+            r, g, b
+        }
+    }
+}

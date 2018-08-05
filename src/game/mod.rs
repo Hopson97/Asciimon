d3231becddbdf4cf8900231931fefd9cfb4349aa
Matthew Hopson <mhopson@hotmail.co.uk>
2018-08-05 10:38:04 +0100

    Move render area constans to new file (#23)

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,5 +1,6 @@
 mod game_state;
 mod player;
+mod render_constants;
 
 pub mod console;
 pub mod world;
@@ -9,44 +10,11 @@ use util::vector::Vector2D;
 
 use self::console::Console;
 use self::game_state::{state_explore::StateExplore, GameState};
+use self::render_constants::*;
 
 use std::io::{stdin, stdout, Write};
 
-pub const SCREEN_SIZE: Vector2D<i32> = Vector2D { x: 120, y: 52 };
 
-pub const LOGO_POSITION: Vector2D<i32> = Vector2D { x: 0, y: 0 };
-pub const LOGO_SIZE: Vector2D<i32> = Vector2D { x: 45, y: 6 };
-
-pub const GAME_AREA_POSITION: Vector2D<i32> = Vector2D {
-    x: 0,
-    y: LOGO_SIZE.y + 1,
-};
-pub const GAME_AREA_SIZE: Vector2D<i32> = Vector2D {
-    x: SCREEN_SIZE.x - (CONSOLE_SIZE.x + 2),
-    y: SCREEN_SIZE.y - (LOGO_SIZE.y + 1),
-};
-pub const GAME_AREA_CENTRE: Vector2D<i32> = Vector2D {
-    x: GAME_AREA_SIZE.x / 2,
-    y: GAME_AREA_SIZE.y / 2,
-};
-
-pub const INPUT_FIELD_POSITION: Vector2D<i32> = Vector2D {
-    x: LOGO_SIZE.x + 2,
-    y: 0,
-};
-pub const INPUT_FIELD_SIZE: Vector2D<i32> = Vector2D {
-    x: SCREEN_SIZE.x - (LOGO_SIZE.x + 2) - (CONSOLE_SIZE.x + 2),
-    y: LOGO_SIZE.y,
-};
-
-pub const CONSOLE_POSITION: Vector2D<i32> = Vector2D {
-    x: SCREEN_SIZE.x - CONSOLE_SIZE.x,
-    y: 0,
-};
-pub const CONSOLE_SIZE: Vector2D<i32> = Vector2D {
-    x: 32,
-    y: SCREEN_SIZE.y,
-};
 
 mod colours {
     use graphics::colour::Colour;

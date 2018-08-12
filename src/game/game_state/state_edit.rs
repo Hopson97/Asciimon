978671705d37cff40b84770082532fdaae8487ea
Hopson97 <mhopson@hotmail.co.uk>
2018-08-12 11:26:15 +0100

    State map editor: Chunk loading works

--- src/game/game_state/state_edit.rs
+++ src/game/game_state/state_edit.rs
@@ -6,16 +6,21 @@ use graphics::Renderer;
 
 use game::Console;
 
+use game::world::Chunk;
+
 use util::{Vector2D};
 
 pub struct StateEdit {
+    loaded_chunks: Vec<Chunk>,
+    view_point: Vector2D<i32>,
     
 }
 
 impl StateEdit {
     pub fn new() -> StateEdit {
         StateEdit {
-            
+            loaded_chunks: Vec::with_capacity(2),
+            view_point: Vector2D::new(0, 0),
         }
     }
 }
@@ -32,10 +37,30 @@ impl GameState for StateEdit {
         console: &mut Console,
     ) -> Option<UpdateResult>
     {
+        console.write(&format!("Args: {:?}", command_args));
+        match command_args {
+            ["load"] => {console.write("loaing and that");},
+            ["load", "map", x, y] => {
+                console.write(&format!("Loading map at ({}, {})", x, y));
+                //TODO: ERROR CHECK
+                let x = x.parse::<i32>().unwrap();
+                let y = y.parse::<i32>().unwrap();
+
+                if let Some(chunk) = Chunk::load(Vector2D::new(x, y)) {
+                    self.loaded_chunks.push(chunk);
+                } else {
+                    console.write(&format!("Map chunk at ({}, {}) does not exist!", x, y));
+                }
+            },
+            _ => {}
+        }
         None
     }
 
     fn draw(&mut self, renderer: &mut Renderer, console: &mut Console) {
-
+        if !self.loaded_chunks.is_empty() {
+            let chunk = self.loaded_chunks.last_mut().unwrap();
+            chunk.render(renderer, self.view_point);
+        }
     }
 }
